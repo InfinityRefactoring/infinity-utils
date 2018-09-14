f3ede4ee2483358973f92684d825105b81401a8b
Thomás Sousa Silva (ThomasSousa96) <thomas.sousa.96@gmail.com>
2018-09-13 23:00:24 -0300

    Add methods for handle localized files

--- src/main/java/com/infinityrefactoring/util/io/Resources.java
+++ src/main/java/com/infinityrefactoring/util/io/Resources.java
@@ -12,14 +12,63 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Enumeration;
 import java.util.HashSet;
+import java.util.Locale;
 import java.util.Set;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
+import java.util.stream.Stream;
 
 /**
  * @author Thomás Sousa Silva (ThomasSousa96)
  */
 public class Resources {
+
+	public static Locale getLocaleOfFilename(String filename, String suffix) {
+		if ((suffix != null) && (!suffix.isEmpty()) && filename.endsWith(suffix)) {
+			filename = filename.substring(0, filename.length() - suffix.length());
+		}
+		int indexOfHyphen = filename.indexOf('_');
+		if ((indexOfHyphen < 0) || ((filename.length() - indexOfHyphen) < 2)) {
+			return null;
+		}
+		String languageTag = filename.substring(indexOfHyphen + 1).replace('_', '-');
+		return Locale.forLanguageTag(languageTag);
+	}
+
+	/**
+	 * Returns the properties file name.
+	 * <h1>Examples:</h1>
+	 * <ul>
+	 * <li>getPropertiesFileName("messages", null, ".properties") returns "messages.properties"</li>
+	 * <li>getPropertiesFileName("messages", Locale.forLanguageTag("pt-BR"), ".properties") returns "messages_pt_BR.properties"</li>
+	 * </ul>
+	 *
+	 * @param baseName the properties file name
+	 * @param locale the locale
+	 * @return If the locale is null,then returns baseName.properties else returns baseName_LanguageTag.properties
+	 */
+	public static String getLocalizedFilename(String baseName, Locale locale, String suffix) {
+		if (baseName == null) {
+			throw new IllegalArgumentException("The baseName cannot be null.");
+		} else if (locale != null) {
+			baseName += ('_' + locale.toLanguageTag().replace('-', '_'));
+		}
+		return ((suffix == null) ? baseName : (baseName + suffix));
+	}
+
+	public static Stream<String> getLocalizedResourceNames(Set<String> resources, Locale locale, String suffix) {
+		return resources.stream()
+				.map(baseName -> getLocalizedFilename(baseName, locale, suffix));
+	}
+
+	public static Stream<URL> getLocalizedResources(Set<String> resources, Locale locale, String suffix) {
+		return getLocalizedResources(resources, locale, suffix, Resources.class.getClassLoader());
+	}
+
+	public static Stream<URL> getLocalizedResources(Set<String> resources, Locale locale, String suffix, ClassLoader classLoader) {
+		return getLocalizedResourceNames(resources, locale, suffix)
+				.flatMap(name -> Resources.getResources(classLoader, name).stream());
+	}
 
 	public static URL getResource(String name) {
 		return Resources.class.getClassLoader().getResource(name);
